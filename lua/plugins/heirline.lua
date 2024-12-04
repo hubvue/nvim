899e44d3050bee8216861599e79946fb4e1a5c9a
hubvue <cckcc.kim@gmail.com>
2024-12-04 19:46:54 +0800

    feat: update

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,6 @@
+if true then return {} end
+
+return {
+  "rebelot/heirline.nvim",
+  opts = function(_, opts) opts.winbar = nil end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,8 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "rust",
+      "typescript",
       -- add more arguments for adding more treesitter parsers
     },
   },
