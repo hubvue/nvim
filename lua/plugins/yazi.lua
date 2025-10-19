4774831f62713e609c824716066978689954e8bd
hubvue <cckcc.kim@gmail.com>
2025-10-19 22:09:28 +0800

    chore: update

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,10 +1,16 @@
 ---@type LazySpec
 return {
   "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
   event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
   keys = {
+    -- 👇 in this section, choose your own keymappings!
     {
       "<leader>-",
+      mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
@@ -15,14 +21,12 @@ return {
       desc = "Open the file manager in nvim's working directory",
     },
     {
-      -- NOTE: this requires a version of yazi that includes
-      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
       "<c-up>",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
   },
-  ---@type YaziConfig
+  ---@type YaziConfig | {}
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
@@ -30,4 +34,11 @@ return {
       show_help = "<f1>",
     },
   },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
