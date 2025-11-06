5be60a971c49c7b3b37dbd9a9b51cafb83bff174
kimwangchong <kimwangchong@didiglobal.com>
2025-11-06 14:14:24 +0800

    feat: theme

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
